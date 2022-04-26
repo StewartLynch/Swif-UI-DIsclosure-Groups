09df0d71f5b2bc363a0aa02614b7db1e12ea4d0f
Stewart Lynch <slynch@createchsol.com>
2022-04-26 16:06:35 -0700

    Initial Commit

---
+++ DisclosureGroup List/DisclosureGroup_ListApp.swift
@@ -0,0 +1,19 @@
+//
+// Created for DisclosureGroup List
+// by Stewart Lynch on 2022-04-26
+// Using Swift 5.0
+//
+// Follow me on Twitter: @StewartLynch
+// Subscribe on YouTube: https://youTube.com/StewartLynch
+//
+
+import SwiftUI
+
+@main
+struct DisclosureGroup_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
