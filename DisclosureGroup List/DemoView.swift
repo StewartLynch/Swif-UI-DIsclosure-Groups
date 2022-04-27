01769d4f3e4fafcaa88a6f0a2e7dc889018ebb86
Stewart Lynch <slynch@createchsol.com>
2022-04-27 13:22:18 -0700

    AddedDemoView

---
+++ DisclosureGroup List/DemoView.swift
@@ -0,0 +1,22 @@
+//
+// Created for DisclosureGroup List
+// by Stewart Lynch on 2022-04-27
+// Using Swift 5.0
+//
+// Follow me on Twitter: @StewartLynch
+// Subscribe on YouTube: https://youTube.com/StewartLynch
+//
+
+import SwiftUI
+
+struct DemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        DemoView()
+    }
+}
